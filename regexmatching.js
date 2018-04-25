cab82e12e78f3842836241d4d55306c47daff3fa
David Seidenberg <davidseidenberg@gmail.com>
2018-04-25 15:42:32 -0700

    Working on regex matching problem

--- regexmatching.js
+++ regexmatching.js
@@ -1,32 +1,47 @@
 var isMatch = function(s, p) {
-  let stringIndex = 0;
+  // make sIndex
+  // loop through p
+  // pChar
+  // pNext
+  // if pChar === s[sIndex] && pNext === *
+    // while s[sIndex] === pChar 
+      // sIndex++
+  // if pChar === a-z && pChar !== s[sIndex] pNext === *
+    // sIndex++
+  // if pChar === s[sIndex] && pNext !== *
+    // sIndex++
+  // if pChar === . && pNext === *
+    // sChar
+    // while s[sIndex] === sChar
+      // sIndex++
+  // if pChar === . && pNext !== *
+    // sIndex++
+
+  let sIndex = 0;
   for (let i = 0; i < p.length; i++) {
     let pChar = p[i];
-    if (pChar >= 'a' && pChar <= 'z') {
-      if (p[i + 1] !== '*') {
-        if (s[stringIndex] === pChar) {
-          stringIndex++;
-        } else {
-          return false;
-        }
+    let pNext = p[i + 1];
+
+    if (pChar === s[sIndex] && pNext === '*') {
+      while (s[sIndex] === pChar) {
+        sIndex++;
       }
-      if (p[i + 1] === '*') {
-        while (s[stringIndex] === pChar && s[stringIndex] !== undefined) {
-          stringIndex++;
-        }
+    } else if (pChar >= 'a' && pChar <= 'z' && pChar !== s[sIndex] && pNext === '*') {
+      sIndex++;
+    } else if (pChar === s[sIndex] && pNext !== '*') {
+      sIndex++;
+    } else if (pChar === '.' && pNext === '*') {
+      let sChar = s[sIndex];
+      while (s[sIndex] === sChar) {
+        sIndex++;
       }
-    }
-    if (pChar === '.') {
-      if (p[i + 1] === '*') {
-        return true
-      } else {
-        stringIndex++;
-      }
+    } else if (pChar === '.' && pNext !== '*') {
+      sIndex++;
+    } else {
+      return false;
     }
   }
-  if (stringIndex === s.length) {
-    return true;
-  } else {
-    return false;
-  }
+
+  return true;
+
 };
