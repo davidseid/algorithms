ae8ca4a04c15f5a7d9fd076932fb1eafd1eec0a9
David Seidenberg <davidseidenberg@gmail.com>
2018-05-22 22:23:08 -0700

    Implement reverseLinkedList

--- reverseList.js
+++ reverseList.js
@@ -1,3 +1,13 @@
 const reverseList = (head) => {
-  
+
+  let prev = null;
+  let curr = head;
+
+  while (curr !== null) {
+    let tempNext = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = tempNext;
+  }
+  return prev;
 }
