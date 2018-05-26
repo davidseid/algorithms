094593e3986e8721a924b85745646d1d5e837fa4
David Seidenberg <davidseidenberg@gmail.com>
2018-05-26 10:31:49 -0700

    Implement mergeTwoLists

--- mergeTwoLists.js
+++ mergeTwoLists.js
@@ -26,7 +26,36 @@ const mergeTwoLists = (l1, l2) => {
     }
   }
   
+  if (p1 === null) {
+    while (p2 !== null) {
+      curr.next = p2;
+      curr = curr.next;
+      p2 = p2.next;
+    }
+  } else {
+    while (p1 !== null) {
+      curr.next = p1;
+      curr = curr.next;
+      p1 = p1.next;
+    }
+  }
+
   return head;
 }
 
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
 
+let test1 = new Node(1);
+test1.next = new Node(2);
+test1.next.next = new Node(4);
+
+let test2 = new Node(1);
+test2.next = new Node(3);
+test2.next.next = new Node(4);
+
+console.log(mergeTwoLists(test1, test2));
