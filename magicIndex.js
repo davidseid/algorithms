033711131e5e14a05832aab44612362d7dc00fe3
David Seidenberg <davidseidenberg@gmail.com>
2018-05-27 10:34:47 -0700

    Implement magicIndex

--- magicIndex.js
+++ magicIndex.js
@@ -2,14 +2,29 @@ const findMagicIndexDistinct = (arr) => {
   // given sorted array of distinct integers
   // find example of arr[i] === i;
 
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === i) {
-      return i;
+  let midIndex = arr.length / 2;
+  
+  if (arr[midIndex] === midIndex) {
+    return midIndex;
+  } else {
+    if (arr[midIndex] < midIndex) {
+      return findMagicIndexDistinct(arr.slice(midIndex));
+    } else {
+      return findMagicIndexDistinct(arr.slice(0, midIndex));
     }
   }
-  return -1;
 }
 
-const findMagicIndexNotDistinct = (arr) => {
+const distinctTestArr = [-40, -12, -3, 0, 4, 6, 7, 8];
+const notDistinctTestArr = [-10, -2, 2, 4, 4, 6, 7];
 
-}
+// const findMagicIndexNotDistinct = (arr) => {
+//   let midIndex = arr.length / 2;
+//   if (arr[midIndex] === midIndex) {
+//     return midIndex;
+//   } else {
+//     return findMagicIndexNotDistinct 
+//   }
+
+
+// }
