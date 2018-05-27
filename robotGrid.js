39c0ba1d4c7864d03ea5ab95d647a0b7797fbe67
David Seidenberg <davidseidenberg@gmail.com>
2018-05-26 21:17:40 -0700

    Working on dynamic programming version

--- robotGrid.js
+++ robotGrid.js
@@ -1,21 +1,8 @@
-const findPath = (grid, r = 0, c = 0) => {
-  
-  grid[r][c] = 'path';
-  if (r === grid.length - 1 && c === grid.length - 1) {
-    return grid;
-  }
-
-  nextGrid = JSON.parse(JSON.stringify(grid));
-  
-  if (nextGrid[r + 1] && nextGrid[r + 1][c] === null) {
-    return findPath(nextGrid, r + 1, c);
-  }
-
-  if (nextGrid[r][c + 1] === null) {
-    return findPath(nextGrid, r, c + 1);
-  }
+const findPath = (grid, r = grid.length - 1, c = grid[0].length - 1, path = [], failedPoints = {}) => {
 
 }
+
+
 
 const testGrid = new Array(4);
 for (let i = 0; i < 4; i ++) {
