8a94acd9d99ac357e789e233d91b1ffc2345324d
David Seidenberg <davidseidenberg@gmail.com>
2018-05-27 13:12:19 -0700

    Implement powerSet with clever caching

--- powerSet.js
+++ powerSet.js
@@ -1,21 +1,25 @@
-const findSubsets = (set, subset = [], subsets = []) => {
-  for (let i = 0; i < set.length; i++) {
-    let subset1 = subset.slice();
-    let subset2 = subset.slice();
-    subset2.push(set[i]);
-    let nextSet = set.slice(i + 1);
-    subsets.push(subset1, subset2);
-    findSubsets(nextSet, subset1, subsets);
-    findSubsets(nextSet, subset2, subsets);
+const findSubsets = (set) => {
+  // if set.length === [] return [[]]
+  // if set.length ==== 1 return [[], [i]]
+  // store each in a cache
+  // to findSubsets (n), take each of hte previous sets and add the latest value
+  let memo = {};
+  memo[JSON.stringify([])] = [[]];
+  memo[JSON.stringify(set.slice(0, 1))] = [[], [set[0]]];
+
+  for (let i = 2; i <= set.length; i++) {
+    let lastSubsets = memo[JSON.stringify(set.slice(0, i - 1))];
+    let subsets = lastSubsets.slice();
+    lastSubsets.forEach((subset) => {
+      let copy = subset.slice();
+      copy.push(set[i - 1]);
+      subsets.push(copy);
+    });
+    memo[JSON.stringify(set.slice(0, i))] = subsets;
   }
 
-  let uniq = {};
-  subsets.forEach((subset) => {
-      uniq[JSON.stringify(subset)] = true;
-  });
-  let result = Object.keys(uniq);
-  return result.map((subset) => JSON.parse(subset));
+  return memo[JSON.stringify(set.slice())];
 }
 
-const testSet = [1, 3, 4];
+const testSet = [1, 3, 4, 5];
 findSubsets(testSet);
