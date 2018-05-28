7f0ced9945354da46acd8b273fab6f98d9c03065
David Seidenberg <davidseidenberg@gmail.com>
2018-05-28 14:03:40 -0700

    Working on permutations with Dups

---
+++ CTCI_8/permutationsWithDups.js
@@ -0,0 +1,15 @@
+const getPermutations = (str) => {
+  const permutations = {};
+
+  let i = 1;
+  let substr = str.slice(0, i);
+  let permutations = [str[0]];
+
+  while (i < str.length) {
+    let nextChar = substr[substring.length - 1];
+    let nextPermutations = [];
+    for (let j = 0; j < permutations.length; j++) {
+      
+    }
+  }
+}
