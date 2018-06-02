ee71f9922094dca631e18d16849c20f903d4f974
David Seidenberg <davidseidenberg@gmail.com>
2018-06-02 10:35:19 -0700

    Initial implementation

--- CTCI_8/stackOfBoxes.js
+++ CTCI_8/stackOfBoxes.js
@@ -1,9 +1,35 @@
-const findTallestStack = (boxes) => {
-  let tallestStack = 0;
-
+const findTallestStack = (availBoxes, currStack = [], currHeight = 0, maxHeight = 0) => {
   
+  if (availBoxes.length === 0) {
+      if (currHeight > maxHeight) {
+          maxHeight = currHeight;
+      }
+  }
 
-  return tallestStack;
+  for (let i = 0; i < availBoxes.length; i++) {
+      let nextStack = currStack.slice();
+      let nextHeight = currHeight;
+      let chosenBox = availBoxes[i];
+      nextStack.push(chosenBox);
+      nextHeight += chosenBox.h;
+      let minH = chosenBox.h;
+      let minW = chosenBox.w;
+      let minL = chosenBox.l;
+
+      let remainingBoxes = [];
+
+      for (let j = 0; j < availBoxes.length; j++) {
+          let boxToCheck = availBoxes[j];
+          if (boxToCheck.l > minL && boxToCheck.w > minW && boxToCheck.h > minH) {
+              remainingBoxes.push(boxToCheck);
+          } 
+      }
+
+      maxHeight = findTallestStack(remainingBoxes.slice(), nextStack, nextHeight, maxHeight);
+  }
+
+  return maxHeight;
+
 }
 
 class Box {
@@ -14,8 +40,10 @@ class Box {
   }
 }
 const boxA = new Box(2, 3, 2);
-const boxB = new Box(2, 4, 3);
+const boxB = new Box(4, 4, 3);
 const boxC = new Box(3, 6, 4);
 const boxD = new Box(7, 5, 6);
 const testBoxes = [];
 testBoxes.push(boxA, boxB, boxC, boxD);
+
+console.log(findTallestStack(testBoxes));
