8c4f9317314465f59f6b3071e06d8059933cd9bf
David Seidenberg <davidseidenberg@gmail.com>
2018-06-02 08:56:31 -0700

    First attempt

--- leetcode/countAndSay.js
+++ leetcode/countAndSay.js
@@ -3,18 +3,34 @@ const countAndSay = (n) => {
   // for each in n, update the result, at the end return the value;
 
   let result = "1";
-  if (n === 1) return result; 
-
-  let num = result[0];
-  let count = 0;
-  let index = 0;
-  let nextResult = '';
-
-  while (result[index] === num) {
-    
+  if (n === 1) return result;
+  let nextResult = ''; 
+  
+  for (let i = 1; i <= n; i++) {
+    nextResult = getNextIteration(result);
+    result = nextResult;
   }
-    
+  return result;
 
+}
+
+const getNextIteration = (lastIteration) => {
+  let num = lastIteration[0];
+  let count = 0;
+  let nextIteration = '';
+
+  for (let i = 0; i < lastIteration.length; i++) {
+    if (lastIteration[i] === num) {
+      count++;
+    } else {
+      nextIteration += count;
+      nextIteration += num;
+      num = lastIteration[i];
+      count = 0;
+    }
+  }
+
+  return nextIteration;
 }
 
 console.log(countAndSay(4));
