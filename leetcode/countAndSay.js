2202261a4ac06ba23213baffed3d341003d11dee
David Seidenberg <davidseidenberg@gmail.com>
2018-06-02 08:45:21 -0700

    Retry countAndSay

--- leetcode/countAndSay.js
+++ leetcode/countAndSay.js
@@ -1,21 +1,6 @@
 const countAndSay = (n) => {
   
-  let result = '1';
-  for (let j = 2; j <= n; j++) {
-    let next = '';
-    let count = 1;
-    let num = result[0];
-    for (let i = 1; i <= result.length; i++) {
-      if (num === result[i]) count++;
-      if (num !== result[i]) {
-        next += count + num;
-        num = result[i];
-        count = 0;
-      }
-    }
-    result = next;
-  }
-  return result;
+
 }
 
 console.log(countAndSay(4));
