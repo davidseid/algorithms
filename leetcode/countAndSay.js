b07807fcabc286d833817a7e934e5aae019be586
David Seidenberg <davidseidenberg@gmail.com>
2018-06-02 08:50:40 -0700

    Working on solution

--- leetcode/countAndSay.js
+++ leetcode/countAndSay.js
@@ -1,5 +1,19 @@
 const countAndSay = (n) => {
-  
+  // initiate the first term n === 1 return 1; 
+  // for each in n, update the result, at the end return the value;
+
+  let result = "1";
+  if (n === 1) return result; 
+
+  let num = result[0];
+  let count = 0;
+  let index = 0;
+  let nextResult = '';
+
+  while (result[index] === num) {
+    
+  }
+    
 
 }
 
