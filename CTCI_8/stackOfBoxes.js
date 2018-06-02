2f1bd241a41342455695917e7ba13409752edc47
David Seidenberg <davidseidenberg@gmail.com>
2018-06-02 09:29:11 -0700

    Start implementation of stackOfBoxes

---
+++ CTCI_8/stackOfBoxes.js
@@ -0,0 +1,21 @@
+const findTallestStack = (boxes) => {
+  let tallestStack = 0;
+
+  
+
+  return tallestStack;
+}
+
+class Box {
+  constructor(w, h, l) {
+    this.w = w;
+    this.h = h;
+    this.l = l;
+  }
+}
+const boxA = new Box(2, 3, 2);
+const boxB = new Box(2, 4, 3);
+const boxC = new Box(3, 6, 4);
+const boxD = new Box(7, 5, 6);
+const testBoxes = [];
+testBoxes.push(boxA, boxB, boxC, boxD);
