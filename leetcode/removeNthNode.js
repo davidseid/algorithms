570d50e5d0e55b5f03ae26193549b42e3aa68e93
David Seidenberg <davidseidenberg@gmail.com>
2018-06-13 19:23:32 -0700

    Implement from scratch nth node

--- leetcode/removeNthNode.js
+++ leetcode/removeNthNode.js
@@ -1,30 +1,5 @@
 const removeNthFromEnd = (head, n) => {
-  // go through nodes to count up all of them
-  // go through again and stop at the one just before the nth from the end
-  // point its next to the one just after the nth
-  // return the head
-  let count = getNodeCount(head);
-  let targetCount = count - n;
-
-  count = 0; 
-  let node = head;
-  while (count !== targetCount) {
-    node = node.next;
-    count++;
-  }
   
-  node.next = node.next.next;
-  return head;
-}
-
-const getNodeCount = (head) => {
-  let count = -1;
-  let node = head;
-  while (node !== null) {
-    node = node.next;
-    count++;
-  }
-  return count;
 }
 
 class ListNode {
