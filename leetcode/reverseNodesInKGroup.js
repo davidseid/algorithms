8f68231c77d77dbdf44859c1c37aa572f28033f6
David Seidenberg <davidseidenberg@gmail.com>
2018-06-15 16:02:43 -0700

    Attempt reverse k nodes in linked list

---
+++ leetcode/reverseNodesInKGroup.js
@@ -0,0 +1,21 @@
+/* Goal: reverse nodes k at a time
+  - leave extra nodes as is
+  - k is guaranteed to be a positive integer
+  
+  i: head of a linkedlist, k
+  o: linked list
+  c: O(1) memory
+  e: k = 1, k = length of list, k is or is not factor of list length
+
+*/
+
+const reverseKGroup = (head, k) => {
+  
+}
+
+class ListNode {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
