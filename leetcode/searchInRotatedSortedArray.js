15c94f0b735d9c0f12b4d1883cfc6871e470bf6d
David Seidenberg <davidseidenberg@gmail.com>
2018-06-19 21:02:35 -0700

    Working on pivot location

--- leetcode/searchInRotatedSortedArray.js
+++ leetcode/searchInRotatedSortedArray.js
@@ -1,10 +1,40 @@
 /*
   4 5 6 7 0 1 2
-  
+
+
 */
 
 
 
 const search = (nums, target) => {
   
+  let pivot;
+  let startIndex = 0;
+  let endIndex = nums.length - 1;
+
+  while (pivot === undefined && startIndex !== endIndex) {
+    let start = nums[startIndex];
+    let end = nums[endIndex];
+    let midIndex = Math.floor(startIndex + Math.floor((endIndex - startIndex) / 2));
+    let mid = nums[midIndex];
+    console.log(start, mid, end)
+    
+    if (start > nums[startIndex + 1]) {
+      pivot = startIndex;
+    }
+    if (mid > nums[midIndex + 1]) {
+      pivot = midIndex;
+    }
+
+    if (end < mid) {
+      startIndex = midIndex + 1;
+      endIndex--
+    }
+    if (mid < start) {
+      startIndex++;
+      endIndex = midIndex - 1;
+    }
+  }
 }
+
+console.log(search([4, 5, 6, 7, 0, 1], 0));
