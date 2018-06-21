198a74bbfe316d474ea30d5e0bc099596ca5e03a
David Seidenberg <davidseidenberg@gmail.com>
2018-06-20 17:19:53 -0700

    reimplementing merge sort from scratch

---
+++ mergeSort2.js
@@ -0,0 +1,33 @@
+const mergeSort = (items) => {
+
+  if (items.length === 1) {
+    return items[0];
+  } else {
+    let firstHalf = items.slice(0, Math.floor(items.length / 2));
+    let secondHalf = items.slice(Math.floor(items.length / 2));
+    return merge(mergeSort(firstHalf), mergeSort(secondHalf));
+  }
+}
+
+const merge = (arr1, arr2) => {
+  let leftPointer = 0;
+  let rightPointer = 0;
+
+  let result = [];
+
+  while (leftPointer < arr1.length && rightPointer < arr2.length) {
+    if (arr1[leftPointer] <= arr2[rightPointer]) {
+      result.push(arr1[leftPointer]);
+      leftPointer++;
+    } else {
+      result.push(arr2[rightPointer]);
+      rightPointer++;
+    }
+  }
+
+  if (leftPointer < arr1.length) {
+    result = result.concat(arr1.slice(leftPointer));
+  } else {
+    result = result.concat(arr2.slice(rightPointer));
+  }
+}
