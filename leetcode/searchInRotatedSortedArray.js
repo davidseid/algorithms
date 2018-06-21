6b67966518124f44e71c6b67c1b424d821438795
David Seidenberg <davidseidenberg@gmail.com>
2018-06-20 17:10:26 -0700

    Built rotate search

--- leetcode/searchInRotatedSortedArray.js
+++ leetcode/searchInRotatedSortedArray.js
@@ -1,15 +1,7 @@
-/*
-  4 5 6 7 0 1 2
 
-
-*/
-
-
-
-const search = (nums, target) => {
+const findPivot = (nums) => {
   let startIndex = 0;
   let endIndex = nums.length - 1;
-  let midIndex = startIndex + Math.floor((endIndex - startIndex) / 2);
 
   let pivot;
   if (nums[endIndex] > nums[startIndex]) {
@@ -17,13 +9,50 @@ const search = (nums, target) => {
   }
 
   while (pivot === undefined) {
+    let midIndex = startIndex + Math.floor((endIndex - startIndex) / 2);
     if (nums[startIndex + 1] < nums[startIndex]) {
       pivot = startIndex;
     } else if (nums[midIndex + 1] < nums[midIndex]) {
       pivot = midIndex;
     }
+    
+    if (nums[startIndex] > nums[midIndex]) {
+      endIndex = midIndex;
+    } else if (nums[midIndex] > nums[endIndex]) {
+      startIndex = midIndex;
+    }
+  }
+  return pivot;
+}
 
+
+
+const search = (nums, target) => {
+  const pivot = findPivot(nums);
+
+  if (target <= nums[pivot] && target >= nums[0]) {
+    return binSearch(0, pivot, nums, target);
+  } else if (target >= nums[pivot + 1] && target <= nums[nums.length - 1]) {
+    return binSearch(pivot + 1, nums.length - 1, nums, target);
+  } else {
+    return -1;
   }
 }
 
-console.log(search([3, 4, 5, 6, 1, 2], 0));
+const binSearch = (startIndex, endIndex, nums, target) => {
+  let found = false;
+
+  while (!found) {
+    let midIndex = startIndex + (Math.floor((endIndex - startIndex) / 2));
+    if (target === nums[midIndex]) {
+      return midIndex;
+    } else if (nums[midIndex] < target) {
+      startIndex = midIndex;
+    } else if (nums[midIndex] > target) {
+      endIndex = midIndex;
+    }
+  }
+}
+
+
+console.log(search([4,5,6,7,0,1,2], 0));
