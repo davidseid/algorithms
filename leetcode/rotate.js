da1f3a12d93274d2cfe66f0d73b77da0909ebb39
David Seidenberg <davidseidenberg@gmail.com>
2018-06-27 12:29:21 -0700

    Implement rotate

--- leetcode/rotate.js
+++ leetcode/rotate.js
@@ -1,40 +1,61 @@
 
-
-
 const rotate = (matrix) => {
-  let queue = [];
-  let n = matrix.length;
+  let n = matrix.length - 1; 
 
-  for (let i = 0; i < n; i++) {
-    queue.push(matrix[0][i]);
+  let saved = [];
+  let putBack = [];
+  let level = 0;
+
+  while (level < matrix.length - 1) {
+    
+    for (let i = 0 + level; i < n; i++) {
+      saved.push(matrix[0 + level][i]);
+    }
+    
+    putBack = saved;
+    saved = [];
+    
+    for (let i = 0 + level; i < n; i++) {
+      saved.push(matrix[i][n - level]);
+      matrix[i][n - level] = putBack.shift();
+    }
+    
+    
+    putBack = saved;
+    saved = [];
+    
+    for (let i = n - level; i > 0; i--) {
+      saved.push(matrix[n - level][i]);
+      matrix[n - level][i] = putBack.shift();
+    }
+    
+    
+    
+    putBack = saved;
+    saved = [];
+    
+    for (let i = n - level; i > 0; i--) {
+      saved.push(matrix[i][0 + level]);
+      matrix[i][0 + level] = putBack.shift();
+    }
+    
+    putBack = saved;
+    saved = [];
+    
+    for (let i = 0 + level; i < n; i++) {
+      matrix[0 + level][i] = putBack.shift();
+    }
+
+    level++;
+    n = n - 2;
   }
-
-  for (let i = 0; i < n; i++) {
-    queue.push(matrix[i][n -1]);
-    matrix[i][n - 1] = queue.shift();
-  }
-
-  for (let i = n - 1; i >= 0; i--) {
-    queue.push(matrix[n - 1][i]);
-    matrix[n - 1][i] = queue.shift();
-  }
-  console.log(queue)
-
-  queue.shift();
-
-  for (let i = n - 2; i >= 0; i--) {
-    queue.push(matrix[n - 2][i]);
-    matrix[n - 2][i] = queue.shift();
-  }
-
-
-
 }
-
+  
 let test = [
-  [1,2,3],
-  [4,5,6],
-  [7,8,9]
+  [ 5, 1, 9,11],
+  [ 2, 4, 8,10],
+  [13, 3, 6, 7],
+  [15,14,12,16]
 ]
 
 rotate(test);
