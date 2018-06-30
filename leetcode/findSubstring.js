92a4bbd2ca92e8bb73cf442bfcf81f0d040ba995
David Seidenberg <davidseidenberg@gmail.com>
2018-06-30 09:41:58 -0700

    findsubstring

---
+++ leetcode/findSubstring.js
@@ -0,0 +1,13 @@
+/*
+  iterate through s
+  find a match in words if it exists
+  if it does, remember start index
+  cross off the word and keep going until all words are crossed
+  or until a non match
+  add index to result if completed
+*/
+
+
+const findSubstring = (s, words) => {
+ // try tmrw
+}
