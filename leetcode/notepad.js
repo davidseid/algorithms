ef8bf54674d8676ce2a2e33b2bff4e3b1a063d10
David Seidenberg <davidseidenberg@gmail.com>
2018-07-02 17:32:33 -0700

    Implementing promise

--- leetcode/notepad.js
+++ leetcode/notepad.js
@@ -1,30 +1,43 @@
 const https = require('https');
 
-async function getTitles() {
+const getTitles = () => {
 
   const titles = [];
 
-  const fetch = (page) => {
+
+
+  const fetch = (page, results) => {
     return new Promise((resolve) => {
-      https.get(`https://jsonmock.hackerrank.com/api/movies/search/?Title=waterworld&page=${page}`, (res) => {
+      https.get(`https://jsonmock.hackerrank.com/api/movies/search/?Title=spiderman&page=${page}`, (res) => {
         let rawData = '';
         res.on('data', (chunk) => { rawData += chunk; });
         res.on('end', () => {
           const parsedData = JSON.parse(rawData);
-          resolve(parsedData);
+          for (let i = 0; i < parsedData.data.length; i++) {
+            results.push(parsedData.data[i].Title);
+          }
+          resolve(titles);
         });
       });
     });
   };
-  
-  let page = 1;
 
-  while (page < 3) {
-    await fetch(page)
+  const fetchAll = async (pages, titles) => {
+    for (let i = 0; i < pages; i++) {
+      await fetch(i, titles);
+    }
+    console.log(titles)
+    return titles;
   }
 
-  console.log(titles)
+  return fetchAll(2, titles);
 
+
+
+  
+  // fetch(1, titles)
+  // .then((res) => fetch(2, res))
+  // .then((res) => console.log(titles));
 }
 
 getTitles();
