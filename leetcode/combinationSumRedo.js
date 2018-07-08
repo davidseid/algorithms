495b8de5ddec206cf7655390698c31e34280f750
David Seidenberg <davidseidenberg@gmail.com>
2018-07-08 11:05:23 -0700

    Restructure algorithms files

--- leetcode/combinationSumRedo.js
+++ leetcode/combinationSumRedo.js
@@ -1,29 +1,25 @@
 
 
 
-const combinationSum = (candidates, target, combination = [], startIndex = 0, sum = -1) => {
+const combinationSum = (candidates, target, combination = [], startIndex = 0, sum = 0) => {
   let solutions = [];
 
-  console.log(combination, sum);
 
   if (sum === target) {
-    solutions.push(combination);
+    solutions.push(combination.slice());
   } else if (sum < target) {
     
     for (let i = startIndex; i < candidates.length; i++) {
       let candidate = candidates[i];
 
-      if (sum < 0) {
-        sum = candidate;
-      } else {
-        sum += candidate;
-      }
-      let nextCombo = combination.slice();
-      nextCombo.push(candidate);
-      solutions = solutions.concat(combinationSum(candidates, target, nextCombo, i, sum));
+      sum += candidate;
+      combination.push(candidate);
+      solutions = solutions.concat(combinationSum(candidates, target, combination, i, sum));
       sum -= candidate;
+      combination.pop();
     }
   }
+
 
   return solutions;
 }
