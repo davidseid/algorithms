baa38849b2162a365a47bc071cfda70ead4e3950
David Seidenberg <davidseidenberg@gmail.com>
2018-07-17 14:28:57 -0700

    Fix up algo

--- trappingRainWater.js
+++ trappingRainWater.js
@@ -1,11 +1,35 @@
-const findPools = (height) => {
-  
-}
+
 
 
 const trap = (height) => {
-  // iterate through height to identify pools
-  // iterate through pools to calculate and sum up units
 
+  let totalWater = 0;
+  let inPool = false;
+  let poolLevel;
 
-}
+  for (let i = 0; i < height.length - 1; i++) {
+    let currHeight = height[i];
+    
+    if (!inPool) {
+      let nextHeight = height[i + 1];
+      if (nextHeight < currHeight) {
+        inPool = true;
+        poolLevel = currHeight;
+      }
+    } else if (inPool) {
+
+      if (currHeight >= poolLevel) {
+        inPool = false;
+      } else {
+        totalWater += (poolLevel - currHeight);
+      }
+    }
+  }
+
+  return totalWater;
+  
+};
+
+let testInput = [0,1,0,2,1,0,1,3,2,1,2,1];
+
+console.log(trap(testInput));
