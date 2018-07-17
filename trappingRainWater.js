21b6a7ef042b2e2f678df7db70930046afd68a91
David Seidenberg <davidseidenberg@gmail.com>
2018-07-17 12:10:13 -0700

    Initialize trap rain water problem

---
+++ trappingRainWater.js
@@ -0,0 +1,11 @@
+const findPools = (height) => {
+  
+}
+
+
+const trap = (height) => {
+  // iterate through height to identify pools
+  // iterate through pools to calculate and sum up units
+
+
+}
