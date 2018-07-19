d878fa811c2099f7d97ff6ef5f5424e027226114
David Seidenberg <davidseidenberg@gmail.com>
2018-07-18 22:45:25 -0700

    move trapping rain water

---
+++ leetcode/trappingRainWater.js
@@ -0,0 +1,35 @@
+
+
+
+const trap = (height) => {
+
+  let totalWater = 0;
+  let inPool = false;
+  let poolLevel;
+
+  for (let i = 0; i < height.length - 1; i++) {
+    let currHeight = height[i];
+    
+    if (!inPool) {
+      let nextHeight = height[i + 1];
+      if (nextHeight < currHeight) {
+        inPool = true;
+        poolLevel = currHeight;
+      }
+    } else if (inPool) {
+
+      if (currHeight >= poolLevel) {
+        inPool = false;
+      } else {
+        totalWater += (poolLevel - currHeight);
+      }
+    }
+  }
+
+  return totalWater;
+  
+};
+
+let testInput = [0,1,0,2,1,0,1,3,2,1,2,1];
+
+console.log(trap(testInput));
