2f90c61d09aee67581f766a018169e2cb4d02ccd
David Seidenberg <david.seidenberg@pubnub.com>
2020-05-29 10:00:35 -0700

    Solve maxSubArray with linear algorithm

--- go/algorithms/maxSubArray.go
+++ go/algorithms/maxSubArray.go
@@ -1,5 +1,7 @@
 package algorithms
 
+import "math"
+
 /*
 Input: [-2,1,-3,4,-1,2,1,-5,4],
 Output: 6
@@ -9,5 +11,13 @@ Explanation: [4,-1,2,1] has the largest sum = 6.
 // Find O(n) solution, then try a divide and conquer approach
 
 func MaxSubArray(nums []int) int {
-	return 0
+
+	var maxSoFar float64 = float64(nums[0])
+	var currMax float64 = float64(nums[0])
+
+	for i := 1; i < len(nums); i++ {
+		currMax = math.Max(float64(nums[i]), currMax+float64(nums[i]))
+		maxSoFar = math.Max(currMax, maxSoFar)
+	}
+	return int(maxSoFar)
 }
