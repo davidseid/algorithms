187c2af06cf982d2df6a7a33f02f5d60ba1e76b9
David Seidenberg <david.seidenberg@pubnub.com>
2020-06-01 09:45:00 -0700

    Add note for further study

--- go/algorithms/plusOne.go
+++ go/algorithms/plusOne.go
@@ -2,6 +2,7 @@ package algorithms
 
 // Time Compelixity: O(n)
 // Space Complexity: O(1)
+// Further study: Is there a more efficient way to prepend an integer to a slice in golang?
 
 func PlusOne(digits []int) []int {
 	for i := len(digits) - 1; i >= 0; i-- {
