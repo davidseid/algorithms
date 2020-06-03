3c03dc25085b8c1f508bfe04961b83387203257f
David Seidenberg <david.seidenberg@pubnub.com>
2020-06-03 09:17:35 -0700

    Comment on optimal solution, not yet implemented

--- go/algorithms/setMatrixZeroes.go
+++ go/algorithms/setMatrixZeroes.go
@@ -29,9 +29,16 @@ func zeroRow(row []int) {
 }
 
 // Approach:
-// For each row
-// if rowHasZero
-// zeroRow()
+// Use first row and first column as markers
+// This requires separate variables (2) to mark whether first row and first col have zero
+// Check first row and first col first and update vars
+// Starting from row 2, col 2, iterate through matrix. If a zero is found
+// mark the first row in col and first col in row as zero for later
+// once done, for each zero in the first row, mark the col as zero
+// for each zero in the first col, mark the row as zero
+// zero the first row and col depending on the initial variables
+
+// This approach uses constant space, and does not revisit any indices
 
 // Time Complexity: O(n)
 // Space Complexity: O(1)
