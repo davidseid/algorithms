aaef602f860a9ef0bd619d410645cb4a1a4a44e7
David Seidenberg <david.seidenberg@pubnub.com>
2020-06-03 08:50:14 -0700

    Add setMatrixZeroes problem

---
+++ go/algorithms/setMatrixZeroes.go
@@ -0,0 +1,18 @@
+package algorithms
+
+// https://leetcode.com/problems/set-matrix-zeroes/
+// Given a m x n matrix, if an element is 0, set its entire row and column to 0. Do it in-place.
+// Solve in place
+// Then, solve with constant space
+
+func SetZeroes(matrix [][]int) {
+
+}
+
+// Approach:
+// For each row
+// if rowHasZero
+// zeroRow()
+
+// Time Complexity: O(n)
+// Space Complexity: O(1)
--- go/main.go
+++ go/main.go
@@ -7,6 +7,11 @@ import (
 )
 
 func main() {
-	arr := []int{1, 2, 3}
-	fmt.Println(algorithms.PlusOne(arr))
+	matrix := [][]int{
+		[]int{1, 1, 1},
+		[]int{1, 0, 1},
+		[]int{1, 1, 1},
+	}
+	algorithms.SetZeroes(matrix)
+	fmt.Println(matrix)
 }
