067b19040c68a733666d26892b9f7ea5fbc2fa4a
David Seidenberg <david.seidenberg@pubnub.com>
2020-06-10 08:16:24 -0700

    Wrote subsets test

---
+++ go/algorithms/subsets_test.go
@@ -0,0 +1,27 @@
+package algorithms
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestSubsets(t *testing.T) {
+	input := []int{1, 2, 3}
+
+	expected := [][]int{
+		{3},
+		{1},
+		{2},
+		{1, 2, 3},
+		{1, 3},
+		{2, 3},
+		{1, 2},
+		{},
+	}
+
+	actual := subsets(input)
+
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("Got %v, expected %v", actual, expected)
+	}
+}
