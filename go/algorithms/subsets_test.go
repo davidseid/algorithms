bc915456645b2994305dbdd7b471881df955a55d
David Seidenberg <david.seidenberg@pubnub.com>
2020-06-18 09:41:09 -0700

    Remove python code

--- go/algorithms/subsets_test.go
+++ go/algorithms/subsets_test.go
@@ -41,20 +41,6 @@ Followup:
 - Solve with bitmask
 */
 
-// class Solution:
-//     def subsets(self, nums: List[int]) -> List[List[int]]:
-//         n = len(nums)
-//         output = []
-
-//         for i in range(2**n, 2**(n + 1)):
-//             # generate bitmask, from 0..00 to 1..11
-//             bitmask = bin(i)[3:]
-
-//             # append subset corresponding to that bitmask
-//             output.append([nums[j] for j in range(n) if bitmask[j] == '1'])
-
-//         return output
-
 func subsets(nums []int) [][]int {
 	n := float64(len(nums))
 	output := [][]int{}
