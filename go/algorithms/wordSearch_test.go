359507a555d72536dfc5e17e2b21c3e2c37fd795
David Seidenberg <david.seidenberg@pubnub.com>
2020-06-26 10:16:54 -0700

    Optimize recursion for performance (peeking and returning unless we are farther than one away)

--- go/algorithms/wordSearch_test.go
+++ go/algorithms/wordSearch_test.go
@@ -36,14 +36,18 @@ board and word consists only of lowercase and uppercase English letters.
 1 <= word.length <= 10^3
 
 Followup:
-Optimize by backup cleanup rather than copying matrix each recurse
+Optimize by backup cleanup rather than copying matrix each recurse (CHECK)
+Optimize by changing base case. Rather than attempt to check path on each side,
+if an adjacent square matches, if it ends the word just return, otherwise, continue
 */
 
 func exist(board [][]byte, word string) bool {
 	for row := range board {
 		for col := range board[row] {
-			if isPath(board, word, 0, row, col) == true {
-				return true
+			if board[row][col] == word[0] {
+				if len(word) == 1 || isPath(board, word, 0, row, col) == true {
+					return true
+				}
 			}
 		}
 	}
@@ -52,47 +56,60 @@ func exist(board [][]byte, word string) bool {
 }
 
 func isPath(board [][]byte, word string, i int, row int, col int) bool {
-	if i >= len(word) {
-		return true
-	}
+	originalValue := board[row][col]
+	board[row][col] = 0
+	i++
 
-	if board[row][col] == word[i] {
-		if i == len(word)-1 {
+	defer func() {
+		board[row][col] = originalValue
+	}()
+
+	if row-1 >= 0 {
+		if i == len(word)-1 && board[row-1][col] == word[i] {
 			return true
 		}
 
-		// boardCopy := deepCopyMatrix(board)
-		originalValue := board[row][col]
-		board[row][col] = 0 // blackout board spot to prevent repeats
-
-		defer func() {
-			board[row][col] = originalValue
-		}()
-
-		if row-1 >= 0 {
-			if isPath(board, word, i+1, row-1, col) {
+		if i < len(word)-1 && board[row-1][col] == word[i] {
+			if isPath(board, word, i, row-1, col) {
 				return true
 			}
 		}
+	}
 
-		if col+1 < len(board[row]) {
-			if isPath(board, word, i+1, row, col+1) {
+	if row+1 < len(board) {
+		if i == len(word)-1 && board[row+1][col] == word[i] {
+			return true
+		}
+
+		if i < len(word)-1 && board[row+1][col] == word[i] {
+			if isPath(board, word, i, row+1, col) {
 				return true
 			}
 		}
+	}
 
-		if row+1 < len(board) {
-			if isPath(board, word, i+1, row+1, col) {
+	if col-1 >= 0 {
+		if i == len(word)-1 && board[row][col-1] == word[i] {
+			return true
+		}
+
+		if i < len(word)-1 && board[row][col-1] == word[i] {
+			if isPath(board, word, i, row, col-1) {
 				return true
 			}
 		}
+	}
 
-		if col-1 >= 0 {
-			if isPath(board, word, i+1, row, col-1) {
+	if col+1 < len(board[row]) {
+		if i == len(word)-1 && board[row][col+1] == word[i] {
+			return true
+		}
+
+		if i < len(word)-1 && board[row][col+1] == word[i] {
+			if isPath(board, word, i, row, col+1) {
 				return true
 			}
 		}
-
 	}
 
 	return false
