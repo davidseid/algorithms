81ef5257e5c6d751dee0e0202c03765ddf2b483b
David Seidenberg <david.seidenberg@pubnub.com>
2020-06-26 09:20:30 -0700

    Uncomment remaining tests, only failing because input is mutated

--- go/algorithms/wordSearch_test.go
+++ go/algorithms/wordSearch_test.go
@@ -1,6 +1,7 @@
 package algorithms
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -97,15 +98,29 @@ func TestExists(t *testing.T) {
 		t.Errorf("Expected %s to exist", word)
 	}
 
+	board = [][]byte{
+		[]byte("ABCE"),
+		[]byte("SFCS"),
+		[]byte("ADEE"),
+	}
+
 	word = "SEE"
+
+	fmt.Println(board)
 
 	if !exist(board, word) {
 		t.Errorf("Expected %s to exist", word)
 	}
 
-	// word = "ABCB"
+	board = [][]byte{
+		[]byte("ABCE"),
+		[]byte("SFCS"),
+		[]byte("ADEE"),
+	}
 
-	// if exist(board, word) {
-	// 	t.Errorf("Expected %s to not exist", word)
-	// }
+	word = "ABCB"
+
+	if exist(board, word) {
+		t.Errorf("Expected %s to not exist", word)
+	}
 }
