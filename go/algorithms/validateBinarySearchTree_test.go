044f2a6fffc0226c04389b4cc50d9fe7b1062dbe
David Seidenberg <david.seidenberg@pubnub.com>
2020-07-16 08:27:38 -0700

    Add validateBinarySearchTree problem

---
+++ go/algorithms/validateBinarySearchTree_test.go
@@ -0,0 +1,26 @@
+/*
+98. Validate Binary Search Tree
+
+Source: https://leetcode.com/problems/validate-binary-search-tree/
+
+Given a binary tree, determine if it is a valid binary search tree (BST).
+
+Assume a BST is defined as follows:
+
+The left subtree of a node contains only nodes with keys less than the node's key.
+The right subtree of a node contains only nodes with keys greater than the node's key.
+Both the left and right subtrees must also be binary search trees.
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isValidBST(root *TreeNode) bool {
+
+}
