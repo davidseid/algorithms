057cf34ff93285a4a76eab5686a0275f7fc2f300
David Seidenberg <david.seidenberg@pubnub.com>
2020-07-29 09:07:23 -0700

    Comment on problem, add function signature

--- go/algorithms/nQueens2_test.go
+++ go/algorithms/nQueens2_test.go
@@ -2,6 +2,31 @@ package algorithms
 
 import "testing"
 
+/*
+Problem:
+https://leetcode.com/problems/n-queens-ii/
+The n-queens puzzle is the problem of placing n queens on an n×n chessboard such that no two queens attack each other.
+
+Given an integer n, return the number of distinct solutions to the n-queens puzzle.
+
+Example:
+
+Input: 4
+Output: 2
+Explanation: There are two distinct solutions to the 4-queens puzzle as shown below.
+[
+ [".Q..",  // Solution 1
+  "...Q",
+  "Q...",
+  "..Q."],
+
+ ["..Q.",  // Solution 2
+  "Q...",
+  "...Q",
+  ".Q.."]
+]
+*/
+
 func TestNQueens2(t *testing.T) {
 
 	input := 4
@@ -12,4 +37,8 @@ func TestNQueens2(t *testing.T) {
 	if actual != expected {
 		t.Errorf("Got %d, wanted %d", actual, expected)
 	}
+}
+
+func totalNQueens(n int) int {
+
 }
