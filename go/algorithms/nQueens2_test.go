225d5858b0c296e49516f6c61b40cb116c68a1cf
David Seidenberg <david.seidenberg@pubnub.com>
2020-07-29 09:14:56 -0700

    Initial implementation to count solutions for nqueens

--- go/algorithms/nQueens2_test.go
+++ go/algorithms/nQueens2_test.go
@@ -25,6 +25,10 @@ Explanation: There are two distinct solutions to the 4-queens puzzle as shown be
   "...Q",
   ".Q.."]
 ]
+
+Rationale:
+- Starting on first row, iterate through row
+- For each column, try placing a queen, recurse, clean up
 */
 
 func TestNQueens2(t *testing.T) {
@@ -32,7 +36,7 @@ func TestNQueens2(t *testing.T) {
 	input := 4
 	expected := 2
 
-	actual := solveNQueens(input)
+	actual := totalNQueens(input)
 
 	if actual != expected {
 		t.Errorf("Got %d, wanted %d", actual, expected)
@@ -40,5 +44,61 @@ func TestNQueens2(t *testing.T) {
 }
 
 func totalNQueens(n int) int {
+	board := makeBoard(n)
+	numSolutions := 0
 
+	solveNQueens2(&numSolutions, &board, 0)
+	return numSolutions
+}
+
+func solveNQueens2(numSolutions *int, pBoard *[][]int, row int) {
+	board := *pBoard
+	if row == len(board) {
+		*numSolutions++
+		return
+	}
+
+	for col := 0; col < len(board); col++ {
+		if isValid(&board, row, col) {
+			board[row][col] = 1
+			solveNQueens2(numSolutions, &board, row+1)
+			board[row][col] = 0
+		}
+	}
+}
+
+func isValid2(pBoard *[][]int, row int, col int) bool {
+	board := *pBoard
+	for i := 0; i < row; i++ {
+		if board[i][col] == 1 {
+			return false
+		}
+	}
+
+	for i, j := row-1, col+1; i >= 0 && j < len(board); i, j = i-1, j+1 {
+		if board[i][j] == 1 {
+			return false
+		}
+	}
+
+	for i, j := row-1, col-1; i >= 0 && j >= 0; i, j = i-1, j-1 {
+		if board[i][j] == 1 {
+			return false
+		}
+	}
+	return true
+}
+
+func makeBoard2(n int) [][]int {
+	board := [][]int{}
+
+	for i := 0; i < n; i++ {
+		row := []int{}
+		for i := 0; i < n; i++ {
+			row = append(row, 0)
+		}
+		board = append(board, row)
+	}
+
+	return board
 }
