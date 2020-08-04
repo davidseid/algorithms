3d030972ec0a634c9c260ffaf42d2f596c943cad
David Seidenberg <david.seidenberg@pubnub.com>
2020-08-04 08:43:56 -0700

    Add first test case

--- leetcode/go/algorithms/zigZagLeveLOrder/zigZagLevelOrder_test.go
+++ leetcode/go/algorithms/zigZagLeveLOrder/zigZagLevelOrder_test.go
@@ -1,5 +1,11 @@
 package zigZagLeveLOrder
 
+import (
+	"testing"
+
+	"github.com/go-test/deep"
+)
+
 /*
 103. Binary Tree Zigzag Level Order Traversal
 https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/
@@ -20,3 +26,39 @@ return its zigzag level order traversal as:
   [15,7]
 ]
 */
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func TestZigZagLevelOrder(t *testing.T) {
+	input := &TreeNode{
+		Val: 3,
+		Left: &TreeNode{
+			Val: 9,
+		},
+		Right: &TreeNode{
+			Val: 20,
+			Left: &TreeNode{
+				Val: 15,
+			},
+			Right: &TreeNode{
+				Val: 7,
+			},
+		},
+	}
+
+	expected := [][]int{
+		[]int{3},
+		[]int{20, 19},
+		[]int{15, 7},
+	}
+
+	actual := zigZagLevelOrder(input)
+
+	if diff := deep.Equal(actual, expected); diff != nil {
+		t.Error(diff)
+	}
+}
