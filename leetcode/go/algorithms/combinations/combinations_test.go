e07abc73d334e5f12cf719ce8c1d4b385b275dc2
David Seidenberg <david.seidenberg@pubnub.com>
2020-08-18 08:28:59 -0700

    Write initial test

--- leetcode/go/algorithms/combinations/combinations_test.go
+++ leetcode/go/algorithms/combinations/combinations_test.go
@@ -1,5 +1,11 @@
 package combinations
 
+import (
+	"testing"
+
+	"github.com/go-test/deep"
+)
+
 /*
 77. Combinations
 https://leetcode.com/problems/combinations/
@@ -34,4 +40,23 @@ Constraints:
 
 func combine(n int, k int) [][]int {
 
+}
+
+func TestCombine(t *testing.T) {
+	t.Run("should return all possible combinations of k numbers from 1..n", func(t *testing.T) {
+		expected := [][]int{
+			{2, 4},
+			{3, 4},
+			{2, 3},
+			{1, 2},
+			{1, 3},
+			{1, 4},
+		}
+
+		actual := combine(4, 2)
+
+		if diff := deep.Equal(actual, expected); diff != nil {
+			t.Error(diff)
+		}
+	})
 }
