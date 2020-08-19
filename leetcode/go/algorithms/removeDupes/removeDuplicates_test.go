4bb4131e447e24aed2a42f981732dd4149f0987e
David Seidenberg <david.seidenberg@pubnub.com>
2020-08-19 08:55:33 -0700

    Implement solution

--- leetcode/go/algorithms/removeDupes/removeDuplicates_test.go
+++ leetcode/go/algorithms/removeDupes/removeDuplicates_test.go
@@ -47,7 +47,16 @@ for (int i = 0; i < len; i++) {
 */
 
 func removeDuplicates(nums []int) int {
+	i := 0
 
+	for _, n := range nums {
+		if i < 2 || n > nums[i-2] {
+			nums[i] = n
+			i++
+		}
+	}
+
+	return i
 }
 
 func TestRemoveDuplicates(t *testing.T) {
