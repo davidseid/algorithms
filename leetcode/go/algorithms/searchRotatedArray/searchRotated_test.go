cc9b3b59614bf39f6658e93e826c42ef39694ada
David Seidenberg <david.seidenberg@pubnub.com>
2020-08-20 08:59:17 -0700

    Define function signature

--- leetcode/go/algorithms/searchRotatedArray/searchRotated_test.go
+++ leetcode/go/algorithms/searchRotatedArray/searchRotated_test.go
@@ -23,3 +23,7 @@ Follow up:
 This is a follow up problem to Search in Rotated Sorted Array, where nums may contain duplicates.
 Would this affect the run-time complexity? How and why?
 */
+
+func search(nums []int, target int) bool {
+
+}
