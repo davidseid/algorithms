6c23d6b41a34da9f155486d2322c8f117c9be559
David Seidenberg <david.seidenberg@pubnub.com>
2020-08-21 09:11:11 -0700

    Comment on rationale

--- leetcode/go/algorithms/removeDuplicates/removeDuplicates_test.go
+++ leetcode/go/algorithms/removeDuplicates/removeDuplicates_test.go
@@ -24,6 +24,32 @@ Input: 1->1->1->2->3
 Output: 2->3
 */
 
+/*
+Rationale:
+If the input linkedlist is empty, return nil
+We cannot assume that the result head is the same as the input head in case the first
+element is part of a duplicate. So we need to initialize a fake head, which points to the
+input head.
+
+We then initialize two pointers:
+- pre, which represents the last confirmed non-dupe, starting at the fakehead
+- cur, which represents the current node being examined, starting at the input head
+
+Iterating through the nexted linked list with a for loop,
+we need a nested for loop to iterate through a group of duplicates
+
+We end that inner for loop when the cur and next are different
+
+Then, if the pre (tail) next is equal to the current node, that means we didn't
+hit a duplicate and we can advance the tail
+
+Otherwise, we jump the tail to next non-dupe
+
+Either way, we crawl to the next node
+
+In the end, we return the fakeHead.Next
+*/
+
 /**
  * Definition for singly-linked list.
  * type ListNode struct {
