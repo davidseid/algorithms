da9c06e83f8251ff34d8cf19dbd58ad60f047382
David Seidenberg <david.seidenberg@pubnub.com>
2020-08-25 08:22:02 -0700

    Add initial test case

--- leetcode/go/algorithms/subsets2/subsets2_test.go
+++ leetcode/go/algorithms/subsets2/subsets2_test.go
@@ -1,5 +1,11 @@
 package subsets2
 
+import (
+	"testing"
+
+	"github.com/go-test/deep"
+)
+
 /*
 90. Subsets II
 https://leetcode.com/problems/subsets-ii/
@@ -21,6 +27,27 @@ Output:
   []
 ]
 */
+
+func TestSubsetsWithDup(t *testing.T) {
+	t.Run("should return the power set", func(t *testing.T) {
+		input := []int{1, 2, 2}
+
+		actual := subsetsWithDup(input)
+
+		expected := [][]int{
+			{2},
+			{1},
+			{1, 2, 2},
+			{2, 2},
+			{1, 2},
+			{},
+		}
+
+		if diff := deep.Equal(actual, expected); diff != nil {
+			t.Error(diff)
+		}
+	})
+}
 
 func subsetsWithDup(nums []int) [][]int {
 
