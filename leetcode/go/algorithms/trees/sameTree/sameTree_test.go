451cac4efa9b6e0348e7c96cb95fe6f699697d20
David Seidenberg <david.seidenberg@pubnub.com>
2020-09-03 08:56:06 -0700

    Remove unnecessary else

--- leetcode/go/algorithms/trees/sameTree/sameTree_test.go
+++ leetcode/go/algorithms/trees/sameTree/sameTree_test.go
@@ -49,9 +49,8 @@ func isSameTree(p *TreeNode, q *TreeNode) bool {
 	if p == nil || q == nil {
 		if p == nil && q == nil {
 			return true
-		} else {
-			return false
 		}
+		return false
 	}
 
 	if p.Val == q.Val && isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right) {
