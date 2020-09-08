590b492dd113b9ab99f9cb8220140522c072e31d
David Seidenberg <david.seidenberg@pubnub.com>
2020-09-08 08:25:42 -0700

    Add initial test case"

--- leetcode/go/algorithms/trees/buildTreeInorderPostorder/buildTree_test.go
+++ leetcode/go/algorithms/trees/buildTreeInorderPostorder/buildTree_test.go
@@ -1,5 +1,11 @@
 package buildTreeInorderPostorder
 
+import (
+	"testing"
+
+	"github.com/go-test/deep"
+)
+
 /*
 106. Construct Binary Tree from Inorder and Postorder Traversal
 
@@ -31,4 +37,23 @@ type TreeNode struct {
 
 func buildTree(inorder []int, postorder []int) *TreeNode {
 
+}
+
+func TestBuildTree(t *testing.T) {
+	t.Run("should build a binary tree from inorder and postorder arrays", func(t *testing.T) {
+		inorder := []int{9, 3, 15, 20, 7}
+		postorder := []int{9, 15, 7, 20, 3}
+
+		actual := buildTree(inorder, postorder)
+
+		expected := &TreeNode{
+			Val:   3,
+			Left:  &TreeNode{Val: 9},
+			Right: &TreeNode{Val: 20, Left: &TreeNode{Val: 15}, Right: &TreeNode{Val: 7}},
+		}
+
+		if diff := deep.Equal(actual, expected); diff != nil {
+			t.Error(diff)
+		}
+	})
 }
