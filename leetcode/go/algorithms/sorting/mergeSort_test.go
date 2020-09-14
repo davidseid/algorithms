c6de0ef646a074ebc2946898689cf465da04641b
David Seidenberg <david.seidenberg@pubnub.com>
2020-09-14 08:27:15 -0700

    Fix initial test case

--- leetcode/go/algorithms/sorting/mergeSort_test.go
+++ leetcode/go/algorithms/sorting/mergeSort_test.go
@@ -22,11 +22,11 @@ func TestMergeSort(t *testing.T) {
 	t.Run("should sort an array without dupes", func(t *testing.T) {
 		input := []int{5, 3, 1, -3, 9, 6, 7}
 
-		actual := mergeSort(input)
+		mergeSort(input)
 
 		expected := []int{-3, 1, 3, 5, 6, 7, 9}
 
-		if diff := deep.Equal(actual, expected); diff != nil {
+		if diff := deep.Equal(input, expected); diff != nil {
 			t.Error(diff)
 		}
 	})
