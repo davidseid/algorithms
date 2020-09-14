6b1812d3286272010c7713cc1e2fae32a4a9d1a5
David Seidenberg <david.seidenberg@pubnub.com>
2020-09-14 08:32:34 -0700

    Add second test case

--- leetcode/go/algorithms/sorting/mergeSort_test.go
+++ leetcode/go/algorithms/sorting/mergeSort_test.go
@@ -30,4 +30,16 @@ func TestMergeSort(t *testing.T) {
 			t.Error(diff)
 		}
 	})
+
+	t.Run("should sort an array with dupes", func(t *testing.T) {
+		input := []int{5, 3, 9, 1, -3, 9, 6, 7}
+
+		mergeSort(input)
+
+		expected := []int{-3, 1, 3, 5, 6, 7, 9, 9}
+
+		if diff := deep.Equal(input, expected); diff != nil {
+			t.Error(diff)
+		}
+	})
 }
