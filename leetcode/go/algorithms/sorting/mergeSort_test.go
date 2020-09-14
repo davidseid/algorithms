d4c37c0078a1d28382f30041218b582422b2edfe
David Seidenberg <david.seidenberg@pubnub.com>
2020-09-14 09:19:32 -0700

    Fix mergeSort algorithm

--- leetcode/go/algorithms/sorting/mergeSort_test.go
+++ leetcode/go/algorithms/sorting/mergeSort_test.go
@@ -10,44 +10,63 @@ import (
 Merge sort
 */
 
-func mergeSort(arr []int) {
-	if len(arr) == 1 {
-		return
+func mergeSort(arr []int, low int, high int) {
+	var mid int
+
+	if low < high {
+		mid = (low + high) / 2
+		mergeSort(arr, low, mid)
+		mergeSort(arr, mid+1, high)
+		merge(arr, low, mid, high)
 	}
 
-	mid := len(arr) / 2
-	front := arr[:mid]
-	back := arr[mid+1:]
-
-	mergeSort(front)
-	mergeSort(back)
-
-	merge(front, back)
 }
 
-func merge(front []int, back []int) []int {
-	result := []int{}
-
+func merge(arr []int, low int, mid int, high int) {
 	fi := 0
+	front := []int{}
 	bi := 0
+	back := []int{}
 
-	for fi < len(front) || bi < len(back) {
-		if fi < len(front) && front[fi] <= back[bi] {
-			result = append(result, front[fi])
+	for i := low; i <= mid; i++ {
+		front = append(front, arr[i])
+	}
+
+	for i := mid + 1; i <= high; i++ {
+		back = append(back, arr[i])
+	}
+
+	i := low
+
+	for fi < len(front) && bi < len(back) {
+		if front[fi] <= back[bi] {
+			arr[i] = front[fi]
 			fi++
 		} else {
-			result = append(result, back[bi])
+			arr[i] = back[bi]
 			bi++
 		}
+		i++
 	}
-	return result
+
+	for fi < len(front) {
+		arr[i] = front[fi]
+		fi++
+		i++
+	}
+
+	for bi < len(back) {
+		arr[i] = back[bi]
+		bi++
+		i++
+	}
 }
 
 func TestMergeSort(t *testing.T) {
 	t.Run("should sort an array without dupes", func(t *testing.T) {
 		input := []int{5, 3, 1, -3, 9, 6, 7}
 
-		mergeSort(input)
+		mergeSort(input, 0, len(input)-1)
 
 		expected := []int{-3, 1, 3, 5, 6, 7, 9}
 
@@ -59,7 +78,7 @@ func TestMergeSort(t *testing.T) {
 	t.Run("should sort an array with dupes", func(t *testing.T) {
 		input := []int{5, 3, 9, 1, -3, 9, 6, 7}
 
-		mergeSort(input)
+		mergeSort(input, 0, len(input)-1)
 
 		expected := []int{-3, 1, 3, 5, 6, 7, 9, 9}
 
