21d1275ed1f31e599b0bdeea605b43b43108ffcf
David Seidenberg <david.seidenberg@pubnub.com>
2020-09-15 08:36:36 -0700

    Modify signatures to use go slices

--- leetcode/go/algorithms/sorting/quickSort_test.go
+++ leetcode/go/algorithms/sorting/quickSort_test.go
@@ -11,14 +11,15 @@ Quicksort
 
 */
 
-func quickSort(arr []int, low int, high int) {
+func quickSort(arr []int) {
+
 }
 
 func TestQuickSort(t *testing.T) {
 	t.Run("should sort an array without dupes", func(t *testing.T) {
 		input := []int{5, 3, 1, -3, 9, 6, 7}
 
-		quickSort(input, 0, len(input)-1)
+		quickSort(input)
 
 		expected := []int{-3, 1, 3, 5, 6, 7, 9}
 
@@ -30,7 +31,7 @@ func TestQuickSort(t *testing.T) {
 	t.Run("should sort an array with dupes", func(t *testing.T) {
 		input := []int{5, 3, 9, 1, -3, 9, 6, 7}
 
-		quickSort(input, 0, len(input)-1)
+		quickSort(input)
 
 		expected := []int{-3, 1, 3, 5, 6, 7, 9, 9}
 
