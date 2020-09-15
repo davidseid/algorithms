8322c8ef7c281aa44bdc0bae8c10949a2c7218c2
David Seidenberg <david.seidenberg@pubnub.com>
2020-09-15 08:20:03 -0700

    Add test cases

--- leetcode/go/algorithms/sorting/quickSort_test.go
+++ leetcode/go/algorithms/sorting/quickSort_test.go
@@ -1,6 +1,38 @@
 package sorting
 
+import (
+	"testing"
+
+	"github.com/go-test/deep"
+)
+
 /*
 Quicksort
 
 */
+
+func TestQuickSort(t *testing.T) {
+	t.Run("should sort an array without dupes", func(t *testing.T) {
+		input := []int{5, 3, 1, -3, 9, 6, 7}
+
+		quickSort(input, 0, len(input)-1)
+
+		expected := []int{-3, 1, 3, 5, 6, 7, 9}
+
+		if diff := deep.Equal(input, expected); diff != nil {
+			t.Error(diff)
+		}
+	})
+
+	t.Run("should sort an array with dupes", func(t *testing.T) {
+		input := []int{5, 3, 9, 1, -3, 9, 6, 7}
+
+		quickSort(input, 0, len(input)-1)
+
+		expected := []int{-3, 1, 3, 5, 6, 7, 9, 9}
+
+		if diff := deep.Equal(input, expected); diff != nil {
+			t.Error(diff)
+		}
+	})
+}
