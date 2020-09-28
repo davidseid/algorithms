9cfc1ab22375a8a6baedb51608e3adcf7291d3e5
David Seidenberg <david.seidenberg@pubnub.com>
2020-09-28 08:52:41 -0700

    Implement bubble up

--- leetcode/go/datastructures/minHeap.go
+++ leetcode/go/datastructures/minHeap.go
@@ -2,7 +2,7 @@ package datastructures
 
 type minHeap []int
 
-func (mh *minHeap) getIndexOfParent(n int) int {
+func (mh *minHeap) getParent(n int) int {
 	if n == 1 {
 		return -1
 	}
@@ -10,10 +10,26 @@ func (mh *minHeap) getIndexOfParent(n int) int {
 	return n / 2
 }
 
-func (mh *minHeap) getIndexOfYoungerChild(n int) int {
+func (mh *minHeap) getYoungerChild(n int) int {
 	return 2 * n
 }
 
-func (mh *minHeap) getIndexOfOlderChild(n int) int {
+func (mh *minHeap) getOlderChild(n int) int {
 	return (2 * n) + 1
+}
+
+func (mh *minHeap) insert(item int) {
+	*mh = append(*mh, item)
+	mh.bubbleUp(len(*mh) - 1)
+}
+
+func (mh *minHeap) bubbleUp(p int) {
+	if mh.getParent(p) == -1 {
+		return
+	}
+
+	if (*mh)[mh.getParent(p)] > (*mh)[p] {
+		mh.swap(p, mh.getParent(p))
+		mh.bubbleUp(mh.getParent(p))
+	}
 }
