e6534f0f3b098d8338efd2d6ba1ed92c04be75b4
David Seidenberg <david.seidenberg@pubnub.com>
2020-10-14 09:08:59 -0700

    Implement quickSort from scratch

--- leetcode/go/algorithms/sorting/quickSort2_test.go
+++ leetcode/go/algorithms/sorting/quickSort2_test.go
@@ -21,7 +21,7 @@ func TestQuickSort2(t *testing.T) {
 }
 
 func quickSort2(arr []int) []int {
-	p := partition(arr)
+	p := partition2(arr)
 
 	quickSort(arr[:p])
 
@@ -29,4 +29,20 @@ func quickSort2(arr []int) []int {
 		quickSort(arr[p+1:])
 	}
 	return arr
+}
+
+func partition2(arr []int) int {
+	left := 0
+	right := len(arr) - 1
+
+	for i := range arr {
+		if arr[i] < arr[right] {
+			arr[i], arr[left] = arr[left], arr[i]
+			left++
+		}
+	}
+
+	arr[left], arr[right] = arr[right], arr[left]
+
+	return left
 }
