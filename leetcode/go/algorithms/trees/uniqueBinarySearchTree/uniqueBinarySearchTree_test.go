a18c037e40356e94c7a6dcda5d2a625e80eac85c
David Seidenberg <david.seidenberg@pubnub.com>
2020-10-16 08:48:42 -0700

    Add test case and function signature

--- leetcode/go/algorithms/trees/uniqueBinarySearchTree/uniqueBinarySearchTree_test.go
+++ leetcode/go/algorithms/trees/uniqueBinarySearchTree/uniqueBinarySearchTree_test.go
@@ -1,5 +1,7 @@
 package uniqueBinarySearchTree
 
+import "testing"
+
 /*
 96. Unique Binary Search Trees
 https://leetcode.com/problems/unique-binary-search-trees/
@@ -23,5 +25,20 @@ Given n = 3, there are a total of 5 unique BST's:
 Constraints:
 
 1 <= n <= 19
-
 */
+
+func TestUniqueBinarySearchTree(t *testing.T) {
+	input := 3
+
+	actual := numTrees(input)
+
+	expected := 5
+
+	if actual != expected {
+		t.Errorf("Got %s, want %s", actual, expected)
+	}
+}
+
+func numTrees(n int) int {
+
+}
