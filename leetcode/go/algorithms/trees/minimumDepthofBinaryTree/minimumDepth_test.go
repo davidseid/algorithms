d1cab6a46e546e52073463720da3b4a98c5e71c8
David Seidenberg <david.seidenberg@pubnub.com>
2020-11-11 10:06:37 -0700

    Unoptimized bfs solution

--- leetcode/go/algorithms/trees/minimumDepthofBinaryTree/minimumDepth_test.go
+++ leetcode/go/algorithms/trees/minimumDepthofBinaryTree/minimumDepth_test.go
@@ -1,6 +1,9 @@
 package minimumDepthofBinaryTree
 
-import "testing"
+import (
+	"math"
+	"testing"
+)
 
 /*
 111. Minimum Depth of Binary Tree
@@ -88,5 +91,16 @@ func TestMinimumDepth2(t *testing.T) {
 }
 
 func minDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
 
+	left := minDepth(root.Left)
+	right := minDepth(root.Right)
+
+	if left == 0 || right == 0 {
+		return int(math.Max(float64(left), float64(right))) + 1
+	}
+
+	return int(math.Min(float64(left), float64(right))) + 1
 }
