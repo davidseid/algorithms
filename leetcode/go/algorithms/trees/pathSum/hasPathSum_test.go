3e9489d873f351f220903bf2e787485395393f55
David Seidenberg <david.seidenberg@pubnub.com>
2020-11-18 09:45:17 -0700

    Declare tree node struct

--- leetcode/go/algorithms/trees/pathSum/hasPathSum_test.go
+++ leetcode/go/algorithms/trees/pathSum/hasPathSum_test.go
@@ -21,3 +21,9 @@ Given the below binary tree and sum = 22,
 7    2      1
 return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22.
 */
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
