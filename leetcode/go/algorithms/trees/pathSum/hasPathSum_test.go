89d64211cf2227d9de9d67438532516588d05a48
David Seidenberg <david.seidenberg@pubnub.com>
2020-11-18 10:05:30 -0700

    Implement simple solution

--- leetcode/go/algorithms/trees/pathSum/hasPathSum_test.go
+++ leetcode/go/algorithms/trees/pathSum/hasPathSum_test.go
@@ -65,11 +65,19 @@ func TestHasPathSum(t *testing.T) {
 		actual := hasPathSum(tree, 22)
 
 		if actual != expected {
-			t.Errorf("Got %v, expected %s=v", actual, expected)
+			t.Errorf("Got %v, expected %v", actual, expected)
 		}
 	})
 }
 
 func hasPathSum(root *TreeNode, sum int) bool {
+	if root == nil {
+		return false
+	}
 
+	if root.Left == nil && root.Right == nil && sum-root.Val == 0 {
+		return true
+	}
+
+	return hasPathSum(root.Left, sum-root.Val) || hasPathSum(root.Right, sum-root.Val)
 }
