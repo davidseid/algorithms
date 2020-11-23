08ab6693869218ec3f4ca16946188d70e1eb4cfe
David Seidenberg <david.seidenberg@pubnub.com>
2020-11-23 09:50:31 -0700

    Clean up problem statement

--- leetcode/go/algorithms/arrays/singleNumber/singleNumber_test.go
+++ leetcode/go/algorithms/arrays/singleNumber/singleNumber_test.go
@@ -25,13 +25,8 @@ Example 3:
 Input: nums = [1]
 Output: 1
 
-
-
 Constraints:
-
     1 <= nums.length <= 3 * 104
     -3 * 104 <= nums[i] <= 3 * 104
     Each element in the array appears twice except for one element which appears only once.
-
-
 */
