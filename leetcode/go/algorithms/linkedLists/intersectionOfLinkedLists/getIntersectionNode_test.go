c33b8e17a85c6f9ed51137fde92dcd3a8b2bb6c2
David Seidenberg <david.seidenberg@pubnub.com>
2020-11-24 10:23:30 -0700

    Implement working, efficient solutino

--- leetcode/go/algorithms/linkedLists/intersectionOfLinkedLists/getIntersectionNode_test.go
+++ leetcode/go/algorithms/linkedLists/intersectionOfLinkedLists/getIntersectionNode_test.go
@@ -154,5 +154,28 @@ func TestGetIntersectionNode(t *testing.T) {
 }
 
 func getIntersectionNode(headA, headB *ListNode) *ListNode {
+	if headA == nil || headB == nil {
+		return nil
+	}
 
+	p1 := headA
+	p2 := headB
+
+	for p1 != p2 {
+		p1 = p1.Next
+		p2 = p2.Next
+
+		if p1 == p2 {
+			return p1
+		}
+
+		if p1 == nil {
+			p1 = headB
+		}
+
+		if p2 == nil {
+			p2 = headA
+		}
+	}
+	return p1
 }
