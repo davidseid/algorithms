a929582e693a2a0721a09ae79b4fde7fac16f885
David Seidenberg <david.seidenberg@pubnub.com>
2021-01-18 09:47:41 -0700

    Implement basic twoSum solution in JS

--- leetcode/javascript/twoSum2.js
+++ leetcode/javascript/twoSum2.js
@@ -1,5 +1,15 @@
 function twoSum(nums, target) {
-    
+    const seen = new Map();
+
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+
+        if (seen.has(complement)) {
+            return [seen.get(complement), i];
+        }
+
+        seen.set(nums[i], i);
+    }
 }
 
 // Test
