0b67fed5122773b61955620cf7ee1345ef68bfa0
David Seidenberg <david.seidenberg@pubnub.com>
2021-01-26 09:20:10 -0700

    Add function signature for longestPalindrome solution function

--- leetcode/go/algorithms/strings/longestPalindromicSubstring/longestPalindromicSubstring_test.go
+++ leetcode/go/algorithms/strings/longestPalindromicSubstring/longestPalindromicSubstring_test.go
@@ -27,13 +27,14 @@ Example 4:
 Input: s = "ac"
 Output: "a"
 
-
-
 Constraints:
-
     1 <= s.length <= 1000
     s consist of only digits and English letters (lower-case and/or upper-case),
 */
+
+func longestPalindrome(s string) string {
+
+}
 
 func TestLongestPalindromicSubstring(t *testing.T) {
 	t.Run("should check for longest palindromic substring in string", func(t *testing.T) {
